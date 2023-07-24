d06b30c6b1c06b7d2e3ced438658c520a688cdbb
dinajar <deenajaradat2@gmail.com>
2023-07-24 20:49:57 +0300

    Lab_38

--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -1,69 +1,46 @@
-import Head from "next/head"
+import Head from "next/head";
+import { useState } from "react";
+import CreateForm from '../component/CreateForm';
+import Header from "../component/Header";
+import ReportTable from '../component/ReportTable'
+import Footer from "../component/Footer"; 
+
 
 export default function () {
+
+  const [data, setData] = useState([])
+
+  function dataHandler(event){
+    event.preventDefault();
+    const dataObj = {
+      location : event.target.location.value, 
+      hourly_sales : [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36]
+
+    }
+
+    setData([...data,dataObj])
+  }
   return (
     <>
-      <head>
+      <Head>
         <title> Cookie Stand Admin </title>
-      </head>
-      <body>
-        <header>
-          <Header />
-        </header>
-        <main>
-          <Form />
+      </Head>
 
-          <p className="text-2xl text-center"> Report Table coming soon... </p>
-        </main>
-        <footer className='p-4 mt-8 bg-green-500 text-black font-bold'>
-          &copy; 2023
-        </footer>
+      <Header />
 
-      </body>
+      <main>
+        
+        <CreateForm  handler={dataHandler}/>
+
+        <ReportTable reports={data} />
+
+      </main>
+      <Footer locations={data.length}/>
+      
+
     </>
   )
 }
 
-function Header() {
-  return (
-    <header className='flex items-center justify-between p-4 bg-green-500 text-black font-bold'>
-      <h1 className='text-4xl'> Cookie Stand Admin </h1>
-    </header>
-  )
-}
-
-function Form() {
-  return (
-    <form className="w-1/2 p-4 mx-auto my-4 bg-green-300 text-black">
-
-      <p className="w-full text-4xl text-center mb-4">Create Cookie Stand</p>
-
-      <div className="flex flex-col bg-green-300 text-black">
-        <h3 className="px-2 py-1 bg-green-300 text-black w-full font-bold">Location</h3>
-        <input name="location" className="flex-auto pl-1 ml-2" />
-      </div>
-
-      <div className="flex flex-row mb-4 p-4">
-
-        <div className="flex flex-col bg-green-300 text-black">
-          <h3 className="px-2 py-1 bg-green-300 text-black w-full font-bold">Minimum Customers Per Hour</h3>
-          <input name="minimum_per" className="flex-auto pl-1 ml-2" />
-        </div>
-
-        <div className="flex flex-col bg-green-300 text-black">
-          <h3 className="px-2 py-1 bg-green-300 text-black w-full font-bold">Maximum Customers Per Hour</h3>
-          <input name="maximum_per" className="flex-auto pl-1 ml-2" />
-        </div>
-
-        <div className="flex flex-col bg-green-300 text-black p-1">
-          <h3 className="px-2 py-1 bg-green-300 text-black w-full font-bold">Average Cookies Per Sale</h3>
-          <input name="average" className="flex-auto pl-1 ml-2" />
-        </div>
-
-        <button className="px-2 py-1 bg-green-500 text-black w-full font-bold">Create</button>
-      </div>
-    </form>
 
 
-  )
-}
